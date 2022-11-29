85cdf600da9e88ee82627feb9333653d68429002
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2022-11-29 14:05:04 +0100

    READY SET GO! numero dos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// Make a console application where you can select what day to run.
-Console.WriteLine("Advent Of Code 2022!");
+﻿// TODO: allow the user to enter an integer as an argument, that integer is the day you want to run.
+public class AOC {
+	static void Main(string[] args) {
+  	
+  	// Go through all the args passed into the application from terminal
+  	for (int i = 0; i < args.GetLength(0); i++) {
+			Console.WriteLine(args[i]);
+  	}
+
+		Console.WriteLine("ADVENT OF CODE 2022!"); 
+		
+		// Peak User Experience right here.
+		Console.WriteLine("Press any key to exit..."); 
+		Console.ReadLine();
+	}
+}
+
