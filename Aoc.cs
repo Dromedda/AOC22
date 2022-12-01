5a1008637daa24238e267033fe27fe274e9b6966
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2022-12-01 16:44:29 +0100

    Day 1 Complete!

---
+++ Aoc.cs
@@ -0,0 +1,41 @@
+namespace Aoc {
+	class Main {
+		public static void runDay(int day) {
+			Console.WriteLine("Attempting to run day: " + day); 
+			switch(day) {
+				case 1: 
+					day1(); 
+					break; 	
+				default: 
+					Console.WriteLine("Failed to run the provided day : " + day);
+					break; 
+			}
+		
+		}
+		// Main Function for day 1
+		static void day1() {
+			string[] data = File.ReadAllLines("./data/day1.txt");
+			int sum = 0; 
+			List<int> listOfSums = new List<int>(); 
+
+			for (int i = 0; i < data.GetLength(0); i++) {
+				// check if newline, its a hack, and it works with the data we have. 
+				if (data[i].Length < 1) {
+					listOfSums.Add(sum); 
+					sum = 0; 
+				} else {
+					sum += int.Parse(data[i]); 
+				}
+			}
+
+			listOfSums.Sort(); 
+
+			// Output largest Sum
+			Console.WriteLine("Largest Sum: " + listOfSums[listOfSums.Count-1]); 
+
+			// get the top 3. 
+			int top3 = listOfSums[listOfSums.Count-1] + listOfSums[listOfSums.Count-2] +  listOfSums[listOfSums.Count-3]; 
+			Console.WriteLine("Total of top3: " + top3); 
+		} 
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,64 +1,25 @@
-﻿// TODO: allow the user to enter an integer as an argument, that integer is the day you want to run.
-public class AOC {
-	
-	static void Main(string[] args) {
-  	
-		string selected_day = "none";
+﻿using Aoc;
 
-  	// Go through all the args passed into the application from terminal
-  	// for (int i = 0; i < args.GetLength(0); i++) {
-			// Console.WriteLine(args[i]);
-  	// }
+class AOC {
+	static void Main(string[] args) {
+		string selected_day = "none";
 
 		if (args.GetLength(0) == 0) {
 			Console.WriteLine("ADVENT OF CODE 2022!"); 
-		
 			Console.WriteLine("Select which day to run: (1 -> 25)"); 
-
-			// TODO Validate input.
-		
 			selected_day = Console.ReadLine(); 
-
 		} else {
 			selected_day = args[0];
-
-			// TODO Validate Arg0
 		}
-	
-		// TODO Run the selected_day
-
-		Days.run_day(int.Parse(selected_day)); 	
 
 		Console.WriteLine("YOU SELECTED DAY: " + selected_day); 
+
+		// Run the Selected day
+		Aoc.Main.runDay(int.Parse(selected_day)); 	
 		
 		// Peak User Experience right here.
 		Console.WriteLine("Press enter/return to exit..."); 
 		Console.ReadLine();
 	}
-
 }
 
-public class Days {
-
-	// Runs the corresponding function for the selected day
-	public static void run_day(int day) {
-		Console.WriteLine("TRIED TO RUN DAY:" + day); 	
-
-		// ugly switch case for all the days LUL
-		switch (day) {
-			case 1:	 
-				day1(); 	
-				break; 
-			default: 
-				Console.WriteLine("Failed to select the day"); 	
-				break; 
-		}
-
-	}
-	
-	// Day 1
-	static void day1() {
-		Console.WriteLine("RAN DAY 1");
-	}
-
-}
